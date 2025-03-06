79256335166d307100436f6e8eb34645f044d4cc
Pelmen2025 <cocickauzxleba@yandex.ru>
2025-03-06 12:53:51 +0500

    Update 1.fs

--- 1.fs
+++ 1.fs
@@ -2,28 +2,30 @@ open System
 
 // Функция, которая преобразует список вещественных чисел в список целых
 let truncateNumbers (nums: list<float>) : list<int> =
-    nums |> List.map int  
+    List.map int nums  
 
 // Функция для чтения списка чисел с клавиатуры
 let readNumbersFromInput () : list<float> =
     printf "Введите числа через пробел: " 
     let input = Console.ReadLine()
-    match input with
+    match input with 
     | null -> []  
     | _ ->
         input.Split([|' '; '\t'|], StringSplitOptions.RemoveEmptyEntries)
-        |> List.ofSeq
-        |> List.choose (fun str ->
+        |> List.fold (fun acc str ->
             match Double.TryParse(str) with
-            | (true, num) -> Some num
-            | _ -> None) 
+            | (true, num) -> num :: acc
+            | _ -> acc
+        ) []
+        |> List.rev
 
 [<EntryPoint>]
 let main argv =
     let numbers = readNumbersFromInput()  
     match numbers with
-    | [] -> printfn "Ошибка: введены некорректные данные или пустая строка."
+    | [] -> 
+        printfn "Ошибка: введены некорректные данные или пустая строка."
     | _ ->
         let truncatedNumbers = truncateNumbers numbers  
         printfn "Результат: %A" truncatedNumbers  
-    0  
+    0   
