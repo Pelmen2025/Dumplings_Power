7f6f0f7e2a181c579454a9581d8e74cc90c17007
Pelmen2025 <cocickauzxleba@yandex.ru>
2025-03-19 12:42:44 +0500

    Create 2.fs

---
+++ 2.fs
@@ -0,0 +1,33 @@
+open System
+
+// Функция для запроса у пользователя количества строк с проверкой ввода
+let rec readInt () =
+    printf "Введите количество строк: " 
+    match Int32.TryParse(Console.ReadLine()) with 
+    | (true, value) when value > 0 -> value 
+    | _ -> 
+        printfn "Ошибка! Введите положительное число." 
+        readInt () 
+
+// Функция для запроса строк с ленивым вычислением
+let rec readLines count =
+    seq {
+        if count > 0 then
+            printf "Введите строку: "
+            yield Console.ReadLine()
+            yield! readLines (count - 1)
+    }
+
+// Основная программа
+[<EntryPoint>]
+let main argv =
+    let count = readInt () 
+    let lines = readLines count 
+    
+    let evenLengthCount =
+        lines
+        |> Seq.filter (fun line -> String.length line % 2 = 0)
+        |> Seq.length
+    
+    printfn "Количество строк с чётной длиной: %d" evenLengthCount 
+    0
