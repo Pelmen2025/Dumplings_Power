04f3b9875c293e55f7659a05f8501c366994f58a
Pelmen2025 <cocickauzxleba@yandex.ru>
2025-03-26 18:14:55 +0500

    Update 2.pl

--- 2.pl
+++ 2.pl
@@ -7,31 +7,23 @@ previous_power_of_two(X, P) :-
 
 % Предикат для чтения списка целых чисел с клавиатуры
 read_integer_list(List) :-
-    write('Введите элементы списка через пробел: '),
+    write('Введите положительные числа списка через пробел: '),
     read_line_to_string(user_input, Input),
     split_string(Input, " ", "", StringList),
     maplist(atom_number, StringList, List).
 
-% Основная логика для нахождения всех положительных элементов списка
+% Основная логика для нахождения диапазона степеней двойки
 find_between_powers_of_two :-
     read_integer_list(List),
-    include(positive, List, PositiveList),
-    (PositiveList == [] -> 
-        write('Нет положительных чисел в списке.'), nl
-    ;   
-        min_list(PositiveList, Min),
-        max_list(PositiveList, Max),
-        
-        LowerBound is floor(log(Min) / log(2)),  
-        UpperBound is floor(log(Max) / log(2)) + 1,
-        
-        format('Все положительные числа расположены между степенями двойки: 2^~w и 2^~w.~n', 
-               [LowerBound, UpperBound])
-    ).
-
-% Проверка на положительное число
-positive(X) :-
-    X > 0.
+    List \= [], % Проверяем, что список не пустой
+    min_list(List, Min),
+    max_list(List, Max),
+    
+    LowerBound is floor(log(Min) / log(2)),  
+    UpperBound is floor(log(Max) / log(2)) + 1,
+    
+    format('Все числа расположены между степенями двойки: 2^~w и 2^~w.~n', 
+           [LowerBound, UpperBound]).
 
 % Запуск программы
 :- find_between_powers_of_two.
