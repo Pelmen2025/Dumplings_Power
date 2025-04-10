b19a714c51618730dcf19ef7db0e9c4d0d2ee55c
Pelmen2025 <cocickauzxleba@yandex.ru>
2025-04-10 11:54:18 +0500

    Update 1Program.cs

--- C#/LB6/ConsoleApp1/1Program.cs
+++ C#/LB6/ConsoleApp1/1Program.cs
@@ -10,27 +10,27 @@ class Program
         string name = Console.ReadLine();
 
         bool hasStudentCard = InputValidator.GetBooleanInput("Есть ли у вас студенческий билет?");
-        bool hasPass = InputValidator.GetBooleanInput("Есть ли у вас пропуск?");
+        bool hasPass = InputValidator.GetBooleanInput("Есть ли у вас пропуск?")
 
-        // Базовый класс
+        
         AccessInfo access = new AccessInfo(hasStudentCard, hasPass);
         Console.WriteLine("\n[Базовый класс]");
         Console.WriteLine(access.ToString());
         Console.WriteLine(access.HasAccess() ? "Доступ разрешён." : "Доступ запрещён.");
 
-        // Копирование
+        
         AccessInfo copyAccess = new AccessInfo(access);
         Console.WriteLine("\n[Копия базового класса]");
         Console.WriteLine(copyAccess.ToString());
 
-        // Дочерний класс
+        
         ExtendedAccessInfo extendedAccess = new ExtendedAccessInfo(name, hasStudentCard, hasPass);
         Console.WriteLine("\n[Дочерний класс]");
         extendedAccess.DisplayGreeting();
         Console.WriteLine(extendedAccess.ToString());
         extendedAccess.DisplayAccessStatus();
 
-        // Копия дочернего класса
+        
         ExtendedAccessInfo copyExtended = new ExtendedAccessInfo(extendedAccess);
         Console.WriteLine("\n[Копия дочернего класса]");
         Console.WriteLine(copyExtended.ToString());
